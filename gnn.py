5f0ab87a237bd3d3ae3d80c8a6591b20537415c9
Sarith Ravinaka Imaduwage <imaduwagesarith932@gmail.com>
2022-10-21 23:26:47 +0530

    Initial Commit.

---
+++ gnn.py
@@ -0,0 +1,99 @@
+import torch
+from torch_geometric.nn import MessagePassing
+from torch_geometric.nn import global_add_pool, global_mean_pool, global_max_pool, GlobalAttention, Set2Set
+import torch.nn.functional as F
+from torch_geometric.nn.inits import uniform
+
+from conv import GNN_node, GNN_node_Virtualnode
+
+from torch_scatter import scatter_mean
+
+class GNN(torch.nn.Module):
+
+    def __init__(self, num_landmarks, num_layer = 5,num_fnn_layers =1, input_node_dim=1,input_edge_dim = 1, emb_dim = 300, 
+                    gnn_type = 'gin', virtual_node = True, residual = False, drop_ratio = 0.5, JK = "last", graph_pooling = "mean"):
+        '''
+            num_tasks (int): number of labels to be predicted
+            virtual_node (bool): whether to add virtual node or not
+        '''
+
+        super(GNN, self).__init__()
+
+        self.num_layer = num_layer
+        self.drop_ratio = drop_ratio
+        self.JK = JK
+        self.emb_dim = emb_dim
+        self.num_landmarks = num_landmarks
+        self.num_fnn_layers = num_fnn_layers
+        self.graph_pooling = graph_pooling
+        self.input_node_dim = input_node_dim
+        self.input_edge_dim = input_edge_dim
+
+        if self.num_layer < 2:
+            raise ValueError("Number of GNN layers must be greater than 1.")
+
+        ### GNN to generate node embeddings
+        if virtual_node:
+            self.gnn_node = GNN_node_Virtualnode(num_layer, emb_dim, JK = JK, drop_ratio = drop_ratio, residual = residual, gnn_type = gnn_type)
+        else:
+            self.gnn_node = GNN_node(num_layer,input_node_dim=self.input_node_dim,input_edge_dim = self.input_edge_dim, emb_dim=emb_dim, JK = JK, drop_ratio = drop_ratio, residual = residual, gnn_type = gnn_type)
+
+
+        ### Pooling function to generate whole-graph embeddings
+        if self.graph_pooling == "sum":
+            self.pool = global_add_pool
+        elif self.graph_pooling == "mean":
+            self.pool = global_mean_pool
+        elif self.graph_pooling == "max":
+            self.pool = global_max_pool
+        elif self.graph_pooling == "attention":
+            self.pool = GlobalAttention(gate_nn = torch.nn.Sequential(torch.nn.Linear(emb_dim, 2*emb_dim), torch.nn.BatchNorm1d(2*emb_dim), torch.nn.ReLU(), torch.nn.Linear(2*emb_dim, 1)))
+        elif self.graph_pooling == "set2set":
+            self.pool = Set2Set(emb_dim, processing_steps = 2)
+        else:
+            raise ValueError("Invalid graph pooling type.")
+
+        self.preprocess_linear_list = torch.nn.ModuleList()
+
+        self.graph_pred_linear_list = torch.nn.ModuleList()
+
+        # if graph_pooling == "set2set":
+        #     for i in range(max_seq_len):
+        #          self.graph_pred_linear_list.append(torch.nn.Linear(2*emb_dim, self.num_vocab))
+
+        if graph_pooling == "set2set":   
+            self.graph_pred_linear_list.append(torch.nn.Linear(2*emb_dim, self.num_landmarks))   
+        
+        # else:
+        #     for i in range(max_seq_len):
+        #          self.graph_pred_linear_list.append(torch.nn.Linear(emb_dim, self.num_vocab))
+        else:
+            self.graph_pred_linear_list.append(torch.nn.Linear(emb_dim, self.num_landmarks))
+                
+
+
+    def forward(self, batched_data):
+        '''
+            Return:
+                A list of predictions.
+                i-th element represents prediction at i-th position of the sequence.
+        '''
+        # input_dim=batched_data.x.size(0)
+
+        h_node = self.gnn_node(batched_data)
+
+        h_graph = self.pool(h_node, batched_data.batch)
+
+        output = h_graph # initial input is set to the output of the GNN 
+        for fnn_inx in range(self.num_fnn_layers):
+            output = self.graph_pred_linear_list[fnn_inx](output)
+            
+        # pred_list = self.graph_pred_linear_list[i](h_graph)
+        # for i in range(self.max_seq_len):
+        #     pred_list.append(self.graph_pred_linear_list[i](h_graph))
+        # return pred_list
+
+        return output 
+
+if __name__ == '__main__':
+    pass
---
+++ main_pyg.py
@@ -0,0 +1,206 @@
+from re import I
+import torch
+from torch_geometric.loader import DataLoader
+import torch.optim as optim
+import torch.nn.functional as F
+from torchvision import transforms
+from gnn import GNN
+
+from tqdm import tqdm
+import argparse
+import time
+import random
+import numpy as np
+import pandas as pd
+import os
+
+from dataset import PygGraphPropPredDataset
+
+### importing utils
+from utils import augment_edge
+from eval_helper import Evaluator
+
+multicls_criterion = torch.nn.MSELoss()
+
+def train(model, device, loader, optimizer):
+    model.train()
+
+    loss_accum = 0
+    for step, batch in enumerate(tqdm(loader, desc="Iteration")):
+        batch = batch.to(device)
+
+        if batch.x.shape[0] == 1 or batch.batch[-1] == 0:
+            pass
+        else:
+            output = model(batch)
+            loss = 0
+            optimizer.zero_grad()
+            for inx,y in enumerate(batch.y):
+                notNanInx = torch.isnan(y)
+                notTrueInx = [b for b in range(notNanInx.size(0)) if notNanInx[b].item() == False]
+                loss += multicls_criterion(output[inx][notTrueInx].to(torch.float32), batch.y[inx][notTrueInx])
+
+            loss.backward()
+            optimizer.step()
+
+            loss_accum += loss.item()
+
+    print('Average training loss: {}'.format(loss_accum / (step + 1)))
+
+def eval(model, device, loader, evaluator, isLogging=False,**kwargs):
+    model.eval()
+    landmarks_ref_list = []
+    landmarks_pred_list = []
+
+    for step, batch in enumerate(loader):
+        batch = batch.to(device)
+
+        if batch.x.shape[0] == 1:
+            pass
+        else:
+            with torch.no_grad():
+                output = model(batch)
+                pred_to_eval = []
+                target_to_eval = []
+                for inx,y in enumerate(batch.y):
+                    notNanInx = torch.isnan(y)
+                    notTrueInx = [b for b in range(notNanInx.size(0)) if notNanInx[b].item() == False]
+                    # print("----",output[inx])
+                    pred_to_eval.append(output[inx][notTrueInx].tolist())
+                    target_to_eval.append(batch.y[inx][notTrueInx].tolist())
+
+            landmarks_ref_list.extend(target_to_eval)
+            landmarks_pred_list.extend(pred_to_eval)
+
+    input_dict = {"landmark_ref": landmarks_ref_list, "landmark_pred": landmarks_pred_list}
+
+    if isLogging and (kwargs["split"] is not None) and (kwargs["split_idx"] is not None):
+        if kwargs["filedir"] is None:
+            filedir = ""
+        else:
+            filedir =kwargs["filedir"] 
+    
+        torch.save({"landmarks_pred":landmarks_pred_list, "image_id":kwargs["split_idx"]}, os.path.join(filedir,"pred_landmarks_{split}.pt".format(split=kwargs["split"])))
+
+    return evaluator.eval(input_dict)
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='GNN baselines on ogbg-code2 data with Pytorch Geometrics')
+    parser.add_argument('--device', type=int, default=0,
+                        help='which gpu to use if any (default: 0)')
+    parser.add_argument('--gnn', type=str, default='gcn-virtual',
+                        help='GNN gin, gin-virtual, or gcn, or gcn-virtual (default: gcn-virtual)')
+    parser.add_argument('--drop_ratio', type=float, default=0,
+                        help='dropout ratio (default: 0)')
+    parser.add_argument('--num_layer', type=int, default=5,
+                        help='number of GNN message passing layers (default: 5)')
+    parser.add_argument('--emb_dim', type=int, default=300,
+                        help='dimensionality of hidden units in GNNs (default: 300)')
+    parser.add_argument('--batch_size', type=int, default=128,
+                        help='input batch size for training (default: 128)')
+    parser.add_argument('--epochs', type=int, default=25,
+                        help='number of epochs to train (default: 25)')
+    parser.add_argument('--num_workers', type=int, default=0,
+                        help='number of workers (default: 0)')
+    parser.add_argument('--dataset', type=str, default="ogbg-code2",
+                        help='dataset name (default: ogbg-code2)')
+    parser.add_argument('--split_ratio', type=float, default=0.8,
+                    help='Training data split ratio, Test and Val dataset are splitted equally from the rest')
+
+    parser.add_argument('--seed',type=int,default=42)
+
+    parser.add_argument('--rootdir', type=str, default="",
+                        help='root directory to build the dataset')
+    
+    parser.add_argument('--filedir', type=str, default="",
+                        help='filename to output result (default: )')
+    args = parser.parse_args()
+    print(args)
+
+    random.seed(args.seed)
+
+    device = torch.device("cuda:" + str(args.device)) if torch.cuda.is_available() else torch.device("cpu")
+
+    # change here to minimize the full dataset size
+    dataset = PygGraphPropPredDataset(root=args.rootdir,name = args.dataset)[:100]
+
+    num_landmarks = dataset.num_landmarks()
+
+    split_idx= dict()
+
+    
+    print('Using random split')
+    perm = torch.randperm(len(dataset))
+    num_train, num_valid, num_test =int(len(dataset)*args.split_ratio), int(len(dataset)*(1-args.split_ratio)/2), int(len(dataset)*(1-args.split_ratio)/2)
+    split_idx['train'] = perm[:num_train]
+    split_idx['valid'] = perm[num_train:num_train+num_valid]
+    split_idx['test'] = perm[num_train+num_valid:]
+
+
+    ### automatic evaluator. takes dataset name as input
+    #  Set this either to MSE or R2
+    eval_metric = "MSE" 
+    evaluator = Evaluator(type=eval_metric,num_landmarks=num_landmarks)
+
+    train_loader = DataLoader(dataset[split_idx["train"]], batch_size=args.batch_size, shuffle=True, num_workers = args.num_workers)
+    valid_loader = DataLoader(dataset[split_idx["valid"]], batch_size=args.batch_size, shuffle=False, num_workers = args.num_workers)
+    test_loader = DataLoader(dataset[split_idx["test"]], batch_size=args.batch_size, shuffle=False, num_workers = args.num_workers)
+
+
+    if args.gnn == 'gin':
+        model = GNN(num_landmarks = num_landmarks, num_layer = args.num_layer, input_node_dim=1,input_edge_dim = 1, gnn_type = 'gin', emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False).to(device)
+    elif args.gnn == 'gin-virtual':
+        model = GNN(num_landmarks = num_landmarks, num_layer = args.num_layer, input_node_dim=1,input_edge_dim = 1, gnn_type = 'gin', emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True).to(device)
+    elif args.gnn == 'gcn':
+        model = GNN(num_landmarks = num_landmarks, num_layer = args.num_layer, input_node_dim=1,input_edge_dim = 1, gnn_type = 'gcn', emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False).to(device)
+    elif args.gnn == 'gcn-virtual':
+        model = GNN(num_landmarks = num_landmarks, num_layer = args.num_layer, input_node_dim=1,input_edge_dim = 1, gnn_type = 'gcn', emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True).to(device)
+    else:
+        raise ValueError('Invalid GNN type')
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    print(f'#Params: {sum(p.numel() for p in model.parameters())}')
+
+    valid_curve = []
+    test_curve = []
+    train_curve = []
+
+    for epoch in range(1, args.epochs + 1):
+        print("=====Epoch {}".format(epoch))
+        print('Training...')
+        train(model, device, train_loader, optimizer)
+
+        print('Evaluating...')
+
+        if epoch is args.epochs:
+            train_perf = eval(model, device, train_loader, evaluator,isLogging=True,filedir=args.filedir,split="train",split_idx=split_idx["train"])
+            valid_perf = eval(model, device, valid_loader, evaluator,isLogging=True,filedir=args.filedir,split="valid",split_idx=split_idx["valid"])
+            test_perf = eval(model, device, test_loader, evaluator,isLogging=True,filedir=args.filedir,split="test",split_idx=split_idx["test"])
+        else:
+            train_perf = eval(model, device, train_loader, evaluator)
+            valid_perf = eval(model, device, valid_loader, evaluator)
+            test_perf = eval(model, device, test_loader, evaluator)
+
+        print({'Train': train_perf, 'Validation': valid_perf, 'Test': test_perf})
+
+        train_curve.append(train_perf)
+        valid_curve.append(valid_perf)
+        test_curve.append(test_perf)
+
+
+    
+    best_val_epoch = np.argmax(np.array(valid_curve))
+    best_train = max(train_curve)
+    print('Finished training!')
+    print('Best validation score: {}'.format(valid_curve[best_val_epoch]))
+    print('Test score: {}'.format(test_curve[best_val_epoch]))
+
+    
+    result_dict = {'Val': valid_curve[best_val_epoch], 'Test': test_curve[best_val_epoch], 'Train': train_curve[best_val_epoch], 'BestTrain': best_train}
+    torch.save(result_dict, os.path.join(args.filedir,"metric_output.pt"))
+
+
+if __name__ == "__main__":
+    main()
